514f600a6f4d03dd00738e179b8fc3179a35aaba
Liam Vinberg <vinberg.liam@gmail.com>
2026-02-27 22:58:30 +0100

    fix: set explicit sipmon formula version

--- Formula/sipmon.rb
+++ Formula/sipmon.rb
@@ -2,6 +2,7 @@ class Sipmon < Formula
   desc "Terminal usage monitor and account switcher for AI providers"
   homepage "https://github.com/liamvinberg/sipmon"
   url "https://github.com/liamvinberg/sipmon/releases/download/v0.1.1/sipmon-0.1.1-darwin-arm64.tar.gz"
+  version "0.1.1"
   sha256 "b5e995f5e65396e7e633c34185172c0e6a31236d1123ec83ea60d3ff54e97de8"
   license "MIT"
 
